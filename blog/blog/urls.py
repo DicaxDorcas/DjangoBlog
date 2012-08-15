e0abfc11c5a50a415aacdd972f8490b2cfffd1c4
Ross Walker <jolliffeross@gmail.com>
2012-08-15 13:34:42 +0100

    Fixed ghosting headders in the comments section.

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'dblog.views.index'),
     url(r'^(?P<article_id>\d+)/$', 'dblog.views.detail'),
-    url(r'^p/(?P<page_slug>\d+)/$', 'pages.views.page'),
+    # url(r'^p/(?P<page_slug>\d+)$', 'pages.views.page'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^comments/', include('django.contrib.comments.urls')),
 )
