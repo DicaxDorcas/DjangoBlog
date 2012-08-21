ac2b7c2497c1674a59c6d2bd97112406ae30efe0
Ross Walker <jolliffeross@gmail.com>
2012-08-21 12:36:43 +0100

    Basic Testing added

--- blog/dblog/tests.py
+++ blog/dblog/tests.py
@@ -1,16 +1,15 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from dblog.models import Article
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class DblogViewsTestCase(TestCase):
+    def test_index(self):
+        post_1 = Article.objects.create(
+            title='DjangoTest',
+            content='DjangoTest',
+            author='DjangoTest',
+            tags='test,test,'
+        )
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('articles' in resp.context)
+        self.assertEqual([article.pk for article in resp.context['latest_article_list']], [1])
