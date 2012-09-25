8e1cfc1028b04969a4bd8cc40f01f29c6f99cde6
DicaxDorcas <github@rosswalker.me>
2012-09-26 01:48:10 +0400

    Modifications for production

--- blog/project/views.py
+++ blog/project/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, render_to_response, get_object_or_404
 
 def index(request):
     projects = Project.objects.all()
-    return render_to_response("projects/index.html", {'projects' : projects})
+    return render_to_response('projects/index.html', {'projects' : projects})
 
 def detail(request, project_id):
     p = get_object_or_404(Project, pk=project_id)
